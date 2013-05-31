64c4ad1c355501df439dd69048b7d71ed768f8e0
Quinn Rohlf <qrohlf@gmail.com>
2013-05-31 11:10:24 -0700

    stubs

--- Lisst.rb
+++ Lisst.rb
@@ -1,27 +1,45 @@
 require 'sinatra'
 require 'haml'
-require 'Maruku'
 
 get '/' do
-  listfile = File.read("list.txt")
-  @listItems = []
-  File.foreach("list.txt") do |line|
-  	item = line.split(' : ')
-  	@listItems.push(item)
-  end
+  @list = List.new("list.txt");
   haml :index
+end
+
 post '/' do
-		# add item to list
-	end
+	# add item to list
+end
+
+put '/' do
+	#update item in list
+end
+
 delete '/:item' do
-		# delete item from the list
-	end
-update '/:item' do
-		# update item in the list
-	end
+	# delete item from the list
 end
 
 class List
+	extend Forwardable
 	def initialize(path) 
 		@file = File.new(path, "r+")
+	end
+
+	def each(&block)
+		@file.each do |line|
+			yield line.split(' : ')
+		end
+	end
+
+	def create(index, title, content)
+		#add an item to the list
+	end
+
+	def update(index, title, content)
+		#update an item in the list
+	end
+
+	def delete(index)
+		#delete an item from the list
+	end
+	
 end
